268577e7e28f1dca5738f3a6436bced2b2c799d7
Wolf Developer <lupo.andrea.93@gmail.com>
2022-11-12 08:21:33 +0100

    Add events with methods

--- 01.events/app.js
+++ 01.events/app.js
@@ -4,6 +4,14 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    add() {
+      this.counter = this.counter + 10;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+    }
+  }
 });
 
 app.mount('#events');
